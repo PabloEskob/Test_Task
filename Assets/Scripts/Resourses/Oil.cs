ec6e30f67bc4f996a5a72b8d284ba6ee72bdac3d
PabloEsk <pavelae27@mail.ru>
2023-02-22 01:28:28 +0300

    Create logic oil

---
+++ Assets/Scripts/Resourses/Oil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Oil : MonoBehaviour
+{
+    private TextMeshProUGUI _textMeshProUGUI;
+
+    public int Count { get; set; }
+
+    private void Awake()
+    {
+        _textMeshProUGUI = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void Add(int value)
+    {
+        Count += value;
+        _textMeshProUGUI.text = Count.ToString();
+    }
+}
--- Assets/Scripts/UI/ActorUi.cs
+++ Assets/Scripts/UI/ActorUi.cs
@@ -4,14 +4,17 @@ public class ActorUi : MonoBehaviour
 {
     [SerializeField] private UpgradePanel _upgradePanel;
     [SerializeField] private OilPump _oilPump;
+    [SerializeField] private Oil _oil;
 
     private void OnEnable()
     {
         _oilPump.Clicked += _upgradePanel.SetActive;
+        _oilPump.GotOil += _oil.Add;
     }
 
     private void OnDisable()
     {
         _oilPump.Clicked -= _upgradePanel.SetActive;
+        _oilPump.GotOil -= _oil.Add;
     }
-} 
+}
